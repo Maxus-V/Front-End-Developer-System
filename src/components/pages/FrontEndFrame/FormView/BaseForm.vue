0a8787f3f5588e83ae8f790f1953a6a651740963
Maxus-V <1902702614@qq.com>
2023-01-06 21:28:08 +0800

    #Add FEDS-08 更新表单视图

---
+++ src/components/pages/FrontEndFrame/FormView/BaseForm.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ElFormItem } from 'element-plus';
+
+const arr = [1,2,3]
+</script>
+
+<template>
+    <div class="baseForm">
+        <el-form-item v-for="item in arr">
+            {{ item }}
+        </el-form-item>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
