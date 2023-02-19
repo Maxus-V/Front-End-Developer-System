bbf53307204effe14f053c256965117c368572e3
Maxus-V <1902702614@qq.com>
2023-02-19 20:22:46 +0800

    #Fixed FEDS-50 test

--- src/components/pages/FrontEndProgression/WebpackBase/index.vue
+++ src/components/pages/FrontEndProgression/WebpackBase/index.vue
@@ -4,7 +4,13 @@ import { useQRCode } from "@vueuse/integrations/useQRCode"
 import { ElCard, ElInput } from "element-plus";
 
 const text = ref("")
+const count = ref(0)
 const qrCode = useQRCode(text)
+
+const load = () => {
+  count.value += 1;
+
+}
 </script>
 
 <template>
@@ -18,5 +24,17 @@ const qrCode = useQRCode(text)
         <ElInput v-model="text" placeholder="输入内容即可展示二维码" />
         <img v-if="text" :src="qrCode" alt="QR Code" />
       </div>
+    </ElCard>
+    <ElCard> 
+      <template #header>
+        <div class="card-header">
+          无限滚动列表
+        </div>
+      </template>
+      <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+        <li v-for="i in count" :key="i" class="infinite-list-item">
+          {{ `我是第${i}` }}
+        </li>
+      </ul>
     </ElCard>
 </template>
